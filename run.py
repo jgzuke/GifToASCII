00aaaba3c139e16029a800ce16b4a9ce9176f01e
jgzuke <jgzuke@gmail.com>
2015-02-25 22:27:46 -0500

    gifs work

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ from PIL import Image
 import math
 import random
 from bisect import bisect
+import glob, os
 
-weights = [" ", ".'", ".-'", "_-^", "%?[]\/*", "#", "#", "#", "#", "#", "#"]
+weights = [" ", ".'", ".-'", "_-^", "%?[]\/*", "#"]
 a = [False, False, False]#"000"
 b = [True, True, True]#"111"
 c = [False, True, False]#"010"
@@ -17,42 +18,9 @@ bools =[[],
         [[a,a,a,a,a,a,a,a,b],[a,a,a,a,b,a,a,a,a],[b,a,a,a,a,a,a,a,a]],
         [[a,a,a,a,a,a,a,a,b],[a,a,a,a,b,a,a,a,a],[c,f,f,a,a,a,a,a,a]],
         [[h,h,h,b,b,b,g,g,g],[b,b,g,g,c,c,c,c,c],[b,b,d,d,d,d,d,b,b],[b,b,e,e,e,e,e,b,b],[d,d,d,c,c,c,e,e,e],[e,e,e,c,c,c,d,d,d],[a,a,a,b,b,b,a,a,a]],
-        [],
-        [],
-        [],
-        [],
-        [],
         []]
 
-def resize(image, lines):
-    lines *= 10
-    wpercent = (lines/float(image.size[1]))
-    hsize = int((float(image.size[0])*float(wpercent)))
-    return image.resize((hsize,lines), Image.ANTIALIAS)
-
-def removeAlpha(image):
-    image.load()  # needed for split()
-    image.convert("RGBA")
-    if image.mode == "RGBA":
-        background = Image.new('RGB', image.size, (255, 255, 255))
-        background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
-        return background.convert("L")
-    else:
-        return image.convert("L")
-
-def addContrast(image, lighten, contrast):
-    image = image.point(lambda p: p + lighten) 
-    return image.point(lambda p: ((p-128)*contrast)+128)
-
-def getImage(name, lines, lighten, contrast):
-    image = Image.open(name)
-    image = resize(image, lines)
-    image = removeAlpha(image)
-    image = addContrast(image, lighten, contrast)
-    return image
-
-def getASCII(name, lines=40, lighten=0, contrast=2):
-    image = getImage(name, lines, lighten, contrast)
+def getASCII(image):
     myascii="\n"
     for j in range((image.size[1]/10)):
         for i in range((image.size[0]/5)):
@@ -91,12 +59,50 @@ def getASCII(name, lines=40, lighten=0, contrast=2):
     myascii+="\n"
     return myascii
 
+def resize(image, lines):
+    lines *= 10
+    wpercent = (lines/float(image.size[1]))
+    hsize = int((float(image.size[0])*float(wpercent)))
+    return image.resize((hsize,lines), Image.ANTIALIAS)
 
-#print getASCII("adam.jpg", 1600, 0, 2)
-print getASCII("stickman.png", 19, 70, 2)
-#print getASCII("icon.png", 400, 0.6, 6)
-#print getASCII("displaypic.png", 400, 1, 1)
-#print getASCII("test.png", 400, 1, 1)
-#print getASCII("favicon.ico", 400, 1, 1)
+def removeAlpha(image):
+    image.load()  # needed for split()
+    image.convert("RGBA")
+    if image.mode == "RGBA":
+        background = Image.new('RGB', image.size, (255, 255, 255))
+        background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
+        return background.convert("L")
+    else:
+        return image.convert("L")
 
-#print getASCII("icon.png", 800)
+def addContrast(image, lighten, contrast):
+    image = image.point(lambda p: p + lighten) 
+    return image.point(lambda p: ((p-128)*contrast)+128)
+
+def modImage(image, lines, lighten, contrast):
+    image = resize(image, lines)
+    image = removeAlpha(image)
+    image = addContrast(image, lighten, contrast)
+    return image
+
+def ImageToACSII(name, lines=40, lighten=0, contrast=2):
+    image = Image.open(name)
+    image = modImage(image, lines, lighten, contrast)
+    print getASCII(image)
+
+def GIFToACSII(name, lines=40, lighten=0, contrast=2, loops = 1):
+    gif = Image.open(name)
+    for x in xrange(loops):
+        nframes = 0
+        while gif:
+            frame = modImage(gif, lines, lighten, contrast)
+            print getASCII(frame)
+            nframes += 1
+            try:
+                gif.seek(nframes)
+            except EOFError:
+                break;
+        gif.seek(0)
+
+#ImageToACSII("jo2.png", 100, 0, 4) 
+GIFToACSII("snoopy.gif", 20, 0, 1, 3)
